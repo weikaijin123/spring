5c03166af063bd4c87cc6216d510324efe935bfa
weikaijin <weikaijin@163.com>
2019-05-22 00:28:56 +0800

    init muti module project

---
+++ demo-parent/demo-dubbo-client/src/main/java/com/wkj/springboot/demodubboclient/DemoDubboClientApplication.java
@@ -0,0 +1,13 @@
+package com.wkj.springboot.demodubboclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDubboClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDubboClientApplication.class, args);
+    }
+
+}
---
+++ demo-parent/demo-dubbo-server/src/main/java/com/wkj/springboot/demodubboserver/DemoDubboServerApplication.java
@@ -0,0 +1,13 @@
+package com.wkj.springboot.demodubboserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDubboServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDubboServerApplication.class, args);
+    }
+
+}
